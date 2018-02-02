0a35076c6df20a5d447485a73305d04b9e70c14a
slosar1 <lslosar@sjnma.org>
2018-02-02 08:19:29 -0600

    HW1

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HW1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is a great language");
+            string answer = Console.ReadLine();
+            Console.WriteLine(answer);
+        }
+    }
+}
